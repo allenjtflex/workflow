7dcdebf854204a3d36f9069eb672e9ed8ea04277
dragon <dragon@dragon.localdomain>
2017-11-25 12:33:10 +0800

    indexList

--- workflow/urls.py
+++ workflow/urls.py
@@ -19,11 +19,12 @@ from django.conf import settings
 from django.conf.urls import url,include
 from django.contrib import admin
 
-from .views import index
+from .views import IndexList
+
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^$', index),
+    url(r'^$', IndexList.as_view(),name="index"),
     url(r'^customers/', include( 'customers.urls', namespace="customers")),
     url(r'^dailywork/', include( 'dailywork.urls', namespace="dailywork")),
     url(r'^bills/', include( 'bills.urls', namespace="bills")),
--- workflow/views.py
+++ workflow/views.py
@@ -6,9 +6,26 @@ from django.contrib import auth
 from django.contrib.auth.views import login, logout
 from django.contrib.auth.decorators import login_required
 
+from django.views.generic.detail import  DetailView
+from django.views.generic.list import ListView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView, FormView
+from django.core.urlresolvers import reverse_lazy, reverse
+from django.db.models import Q
+
+from bills.models import Bill
 
 
 def index(request):
     title="Mysite Home"
     #return HttpResponse("<h1>%s</h1>" %(title))
     return render( request, 'index.html', locals() )
+
+
+class IndexList(ListView):
+
+
+    def get_queryset(self, *args, **kwargs):
+
+        query_set = Bill.objects.all()
+
+        return query_set
